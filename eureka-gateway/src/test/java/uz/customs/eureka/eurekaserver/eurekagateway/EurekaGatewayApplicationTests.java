0f7b083dd0a36319223a725ce91d31bcbe577470
RustamShoymardonov <rustam_bek.1989r@mail.ru>
2022-10-31 19:34:08 +0500

    configured client eureka spring cloud (31.10.2022)

---
+++ eureka-gateway/src/test/java/uz/customs/eureka/eurekaserver/eurekagateway/EurekaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.customs.eureka.eurekaserver.eurekagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
